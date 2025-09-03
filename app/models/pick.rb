d688dce521f00c203c091da322b445d199c38d9e
Edgar Duran <edgarduran@mac.lan>
2025-09-03 09:50:21 -0600

    wip

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -5,6 +5,11 @@ class Pick < ApplicationRecord
   belongs_to :team
 
   validates :user_id, uniqueness: { scope: [:season_id, :team_id], message: "You already picked this team this season" }
+  validate :pick_in_before_week_kickoff, on: [:create, :update]
+  # validates start_date: { before: :week_start_date, message: "Cannot make or change pick after week has started" }
 
 
+  def pick_in_before_week_kickoff
+    true
+  end
 end
--- spec/models/pick_spec.rb
+++ spec/models/pick_spec.rb
@@ -9,19 +9,37 @@ RSpec.describe Pick, type: :model do
         end_date: Date.new(Date.today.year, Date.today.month + 1, Date.today.day + 1)
       )
     }
-    let(:week) { Week.create(week_number: 1, start_date: DateTime.now + 1.hour, season: season)}
-    let(:team) { Team.create(name: 'Chicago Bears') }
+    let(:week) { Week.create(week_number: 2, start_date: DateTime.now + 2.hour, season: season)}
 
     subject(:pick) { Pick.new(user: user, season: season, week: week, team: team) }
 
     context 'when pick is valid' do
+      let(:team) { Team.create(name: 'Chicago Bears') }
+
       it 'saves user pick' do
         expect(pick.valid?).to be(true)
         expect(pick.save).to be(true)
       end
     end
     context 'when pick is not valid' do
-      # it 'does not save usr pick' do
+      let(:team) { nil }
+      it 'does not save user pick with no team' do
+        expect(pick.valid?).to be(false)
+        expect(pick.save).to be(false)
+      end
+      # it 'does not save user pick when deadline passes' do
+      # end
+      it 'does not save user pick when team picked more than once' do
+        before {
+          let(:last_week) { Week.create(week_number: 1, start_date: DateTime.now - 7.days, season: season)}
+          let(:last_week__team_pick) { Team.create(name: 'Chicago Bears') }
+
+         }
+
+         expect(pick.save).to eq(false)
+        #  assert error message
+      end
+      # it 'does not save user pick with when user has 2 incorrect picks' do
       # end
     end
   end
